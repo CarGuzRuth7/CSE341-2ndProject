64ee7c4b316eadb1b99b6222b312d596cbb2a3dd
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-10-14 20:29:21 -0300

    Add rest file and format/lint documents

--- server.js
+++ server.js
@@ -4,10 +4,7 @@ const mongodb = require('./db/connection');
 const cors = require('cors');
 const port = process.env.PORT || 4000;
 
-app
-  .use(cors())
-  .use(express.json())
-  .use('/', require('./routes'));
+app.use(cors()).use(express.json()).use('/', require('./routes'));
 
 mongodb.initDB((err) => {
   if (err) {
