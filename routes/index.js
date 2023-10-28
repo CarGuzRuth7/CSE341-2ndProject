7687ad8e865635772566ca793c7c008a47dd996a
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-10-28 12:22:02 -0300

    Add auth0 to API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const routes = require('express').Router();
+const { auth, requiresAuth } = require('express-openid-connect');
 
-routes.use('/', require('./swagger'));
+require('dotenv').config();
 
-routes.use('/albums', require('./albums'));
-routes.use('/artists', require('./artists'));
-routes.use('/users', require('./users'));
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+routes.use(auth(config));
+
+routes.get('/', (req, res) => {
+  res.send(
+    req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out. -> /login <- Log in to see the API'
+  );
+});
+
+routes.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+routes.get('/logout', (req, res) => {
+  req.oidc.logout();
+  res.redirect('/');
+});
+
+routes.use('/', requiresAuth(), require('./swagger'));
+
+routes.use('/albums', requiresAuth(), require('./albums'));
+routes.use('/artists', requiresAuth(), require('./artists'));
+routes.use('/users', requiresAuth(), require('./users'));
 
 module.exports = routes;
